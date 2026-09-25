09c1d04967f5cd783781b4fc851a52f30f476709
GustavM01 <gustav.milihagulu@gmail.com>
2026-09-25 11:27:22 +0200

    TicTacToe mobile version and mobile menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Link, Outlet, useLocation } from "react-router-dom";
 import "./Menu.css";
+import { MenuIcon, X } from "lucide-react";
 
 function Menu() {
   const location = useLocation();
 
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location]);
 
   function changeTheme() {
     setTheme((prev) => (prev === "dark" ? "light" : "dark"));
@@ -18,19 +24,15 @@ function Menu() {
 
   return (
     <>
-      {/* <h1>Menu</h1> */}
-      <nav className="navbar">
+      <div className="mobile-menu">
+        <div className="mobile-menu-left" />
+        <button className="menu-btn" onClick={() => setIsOpen((prev) => !prev)}>
+          {!isOpen ? <MenuIcon /> : <X />}
+        </button>
+      </div>
+
+      <nav className={`navbar mobile-navbar ${isOpen ? "open" : ""}`}>
         <div style={{ width: "50px" }} />
-        {/* <Link
-            className={
-              location.pathname === "/"
-                ? "navbar-item btn active"
-                : "navbar-item btn"
-            }
-            to="/"
-          >
-            Home
-          </Link> */}
         <Link
           className={
             location.pathname === "/"
